26c08c8defa6883902028c4e053e3f959a28729d
gitfname <newamailnam@gmail.com>
2023-04-30 00:42:27 +0430

    start coding

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,7 @@
-import H1 from "@/components/Typography/H1"
-import H2 from "@/components/Typography/H2"
-import H3 from "@/components/Typography/H3"
-import H4 from "@/components/Typography/H4"
-import H5 from "@/components/Typography/H5"
 
 export default function PageComponent() {
   return (
-    <div className="pt-8 ">
-      <p>hello world</p>
-      <H1>hello world h1</H1>
-      <H2>hello world h2</H2>
-      <H3>hello world h3</H3>
-      <H4>hello world h4</H4>
-      <H5>hello world h5</H5>
-      hello world and all guys
+    <div>
     </div>
   )
 }
