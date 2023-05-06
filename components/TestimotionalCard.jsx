67cf8cfa5f6021d5cbda2d687302286a522950c6
gitfname <newamailnam@gmail.com>
2023-05-06 23:39:49 +0430

    'TestiMotional' component added

---
+++ components/TestimotionalCard.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { AiFillStar } from "react-icons/ai"
+
+
+export default function Testimotional({username, img, text, tl_text, score}) {
+    return (
+        <div className="w-full rounded-lg bg-black/30 p-5 h-[240px] relative">
+            <p className="text-white/50 text-xs font-light font-[Lexend] tracking-wide absolute top-4 left-4">{tl_text}</p>
+            <Image
+                width={60}
+                height={60}
+                alt=""
+                src={img}
+                className="w-12 h-12 rounded-full absolute top-4 left-1/2 -translate-x-1/2
+                object-center object-cover"
+            />
+            <div className="absolute top-4 right-4 flex items-center gap-x-1">
+                <p className="leading-none text-white/50 text-xs font-light font-[Lexend]">
+                    {score}
+                </p>
+                <AiFillStar className="w-4 h-4 fill-red-500" />
+            </div>
+            <p className="text-white text-sm font-[Lexend] font-light tracking-wide text-center mt-[60px]">
+                {username}
+            </p>
+            <p className="text-white/75 leading-[18px] tracking-wide text-xs font-light font-[Lexend] line-clamp-6 mt-5">
+                {text}
+            </p>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ import SeriesCard_2 from "@/components/SeriesCard_2"
 import animations from "@/lib/db/animations"
 import MovieCard_7 from "@/components/MovieCard_7"
 import FeatureCard from "@/components/FeatureCard"
+import Testimotional from "@/components/TestimotionalCard"
 
 export default function HomePage() {
 
@@ -568,6 +569,35 @@ export default function HomePage() {
           title="Greatest Archive"
           desc="Unlock a World of Cinematic Treasures with Our Great Movie Archive"
           img="/direct.svg"
+        />
+
+      </div>
+
+
+      <div className="mt-[100px] grid grid-cols-3 w-11/12 mx-auto gap-x-20 max-w-6xl">
+
+        <Testimotional
+          username="Skylar Dias"
+          tl_text="Avatar"
+          text="We are greatly helped by the services of the MORENT Application. Morent has low prices and also a wide variety of cars with good and comfortable facilities. In addition, the service provided by the officers is also very friendly and very polite."
+          img="/movie-images/img10.jpg"
+          score="7 / 10"
+        />
+
+        <Testimotional
+          username="Alex Stanton"
+          tl_text="Tetris"
+          text="We are greatly helped by the services of the MORENT Application. Morent has low prices and also a wide variety of cars with good and comfortable facilities. In addition, the service provided by the officers is also very friendly and very polite."
+          img="/movie-images/img11.jpg"
+          score="8 / 10"
+        />
+
+        <Testimotional
+          username="Ervin Howell"
+          tl_text="65"
+          text="When it is doing so, it dances bizarrely. This Pokémon’s black pearls are valuable as works of art. Nidoking’s thick tail packs enormo usly destructive power egac Metrics Producer Legacy Metrics Producer"
+          img="/movie-images/img13.jpg"
+          score="8 / 10"
         />
 
       </div>
