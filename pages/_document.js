8534582523d6f2516d2a0c115e3555c03fcee481
gitfname <newamailnam@gmail.com>
2023-05-09 01:05:46 +0430

    pwa added

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ef4444" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800&family=Raleway:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
